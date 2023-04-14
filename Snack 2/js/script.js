979fa6cb94ed627da001c05d4f806532db12763b
giovannilampis <giolampis@hotmail.com>
2023-04-14 16:43:39 +0200

    an array of teams contains several properties

--- Snack 2/js/script.js
+++ Snack 2/js/script.js
@@ -1,1 +1,63 @@
 "use strict"
+
+// an array of football teams contains several properties: name, scored points, fouls
+
+const soccerTeams = [
+    {
+        name: 'Manchester-United',
+        rankingPoints: 0,
+        fouls: 0,
+    }, {
+        name: 'Southampton',
+        rankingPoints: 0,
+        fouls: 0,
+    }, {
+        name: 'Everton',
+        rankingPoints: 0,
+        fouls: 0,
+    }, {
+        name: 'Leeds',
+        rankingPoints: 0,
+        fouls: 0,
+    }, {
+        name: 'Crystal-Palace',
+        rankingPoints: 0,
+        fouls: 0,
+    }, {
+        name: 'Newcastle',
+        rankingPoints: 0,
+        fouls: 0,
+    }, {
+        name: 'Manchester-City',
+        rankingPoints: 0,
+        fouls: 0,
+    }, {
+        name: 'West-Ham',
+        rankingPoints: 0,
+        fouls: 0,
+    }, {
+        name: 'Chelsea',
+        rankingPoints: 0,
+        fouls: 0,
+    }, {
+        name: 'Liverpool',
+        rankingPoints: 0,
+        fouls: 0,
+    }, {
+        name: 'Tottenham',
+        rankingPoints: 0,
+        fouls: 0,
+    }, {
+        name: 'Aston-Villa',
+        rankingPoints: 0,
+        fouls: 0,
+    }, {
+        name: 'Leichester-City',
+        rankingPoints: 0,
+        fouls: 0,
+    }, {
+        name: 'Arsenal',
+        rankingPoints: 0,
+        fouls: 0,
+    }
+];
