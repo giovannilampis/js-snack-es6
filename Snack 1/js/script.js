76ffea462dd517c27cd6d86ad047fed811966bf9
giovannilampis <giolampis@hotmail.com>
2023-04-14 15:57:53 +0200

    array of objects contains bikes & relative weights

--- Snack 1/js/script.js
+++ Snack 1/js/script.js
@@ -1,1 +1,34 @@
 "use strict"
+
+// an array of objects contains some bikes and relative weights
+
+const bicycles = [
+    {
+        name: Dahon,
+        weight: 13,
+    }, {
+        name: Alex-Moulton,
+        weight: 10,
+    }, {
+        name: Bromptonn,
+        weight: 10.5,
+    }, {
+        name: Wilier,
+        weight: 12,
+    }, {
+        name: Birdy,
+        weight: 9.5,
+    }, {
+        name: Dahon,
+        weight: 13,
+    }, {
+        name: Allen-Sports-Ultra-X,
+        weight: 9.9,
+    }, {
+        name: Helix,
+        weight: 11.5,
+    }, {
+        name: Hummingbird,
+        weight: 14,
+    }
+]
